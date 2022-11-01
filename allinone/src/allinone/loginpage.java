32e9e99040523ceebfc5040a9285cb45004a8209
WannaCry24 <muftimayank@gmail.com>
2022-11-01 23:27:11 +0530

    1

--- allinone/src/allinone/loginpage.java
+++ allinone/src/allinone/loginpage.java
@@ -3,6 +3,10 @@ package allinone;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.FlowLayout;
 
 public class loginpage {
 
@@ -16,9 +20,15 @@ public class loginpage {
 	public loginpage()
 	{
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.getContentPane().setBackground(new Color(128, 128, 192));
+		frame.setBounds(100, 100, 901, 740);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel Login = new JLabel("LOGIN PAGE");
+		Login.setBounds(369, 65, 163, 31);
+		Login.setFont(new Font("Schadow BT", Font.BOLD, 25));
+		frame.getContentPane().add(Login);
 		frame.setVisible(true);
-		frame.setLayout(null);
 	}
 }
